a695bfe87b9068aba75b3ca7077e8c2979ccdfca
EvolutionSO2 <105604331+EvolutionSO2@users.noreply.github.com>
2022-05-15 20:12:58 +0200

    Add files via upload

---
+++ pen-export-WxYNZV/pen-export-WxYNZV/README.md
@@ -0,0 +1,4 @@
+# Drawing Board
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/aundreyd/pen/WxYNZV](https://codepen.io/aundreyd/pen/WxYNZV).
+
---
+++ script.js
@@ -0,0 +1,93 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+//Problem: No user interacyion causes no change to application
+//Solution: User interaction causes changes appropriately
+
+var color = $(".selected").css("background-color");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
+
+//When clicking on control list items
+$(".controls").on("click", "li", function() {
+  //Deselect sibling elements
+  $(this).siblings().removeClass("selected");
+  //Select clicked element
+  $(this).addClass("selected");
+  //cache current color
+  color = $(this).css("background-color");
+});
+
+//When "New Color" is pressed
+$("#revealColorSelect").click(function() {
+  //Show color select or hide the color select
+  changeColor();
+  $("#colorSelect").toggle();
+});
+
+//update the new color span
+function changeColor() {
+  var r = $("#red").val();
+  var g = $("#green").val();
+  var b = $("#blue").val();
+  $("#newColor").css("background-color", "rgb(" + r + "," + g + ", " + b + ")");
+}
+
+//When color sliders change
+$("input[type=range]").change(changeColor);
+
+//When "Add Color" is pressed
+$("#addNewColor").click(function() {
+  //Append the color to the controls ul
+  var $newColor = $("<li></li>");
+  $newColor.css("background-color", $("#newColor").css("background-color"));
+  $(".controls ul").append($newColor);
+  //Select the new color
+  $newColor.click();
+});
+
+//On mouse events on the canvas
+$canvas.mousedown(function(e) {
+  lastEvent = e;
+  mouseDown = true;
+}).mousemove(function(e) {
+  //Draw lines
+  if (mouseDown) {
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = color;
+    context.stroke();
+    lastEvent = e;
+  }
+}).mouseup(function() {
+  mouseDown = false;
+}).mouseleave(function() {
+  $canvas.mouseup();
+});
